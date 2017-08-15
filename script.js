627722fc4631da5fad4b50b7f3747cc784886531
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-14 19:11:58 -0500

    Started new default gatsby starter

--- script.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    let work = document.getElementById('Work');
-        
-    work.addEventListener('click', event => {
-        if (event.target.id[0] === '/') {
-            window.open(`https://shoemakerdr.github.io${event.target.id}`, '_blank');
-        }
-   });
-});
