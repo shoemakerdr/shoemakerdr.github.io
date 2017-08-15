df50815dbe667442c1f95e55ef1d0d69c801b87e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-14 20:44:12 -0500

    updated gatsby version

---
+++ .cache/layouts/index.js
@@ -0,0 +1,7 @@
+
+  import React from "react"
+  import Component from "/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/src/layouts/index.js"
+  import data from "/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/layout-index.json"
+
+  export default (props) => <Component {...props} {...data} />
+  
---
+++ .cache/sync-requires.js
@@ -0,0 +1,27 @@
+// prefer default export if available
+const preferDefault = m => m && m.default || m
+
+
+exports.components = {
+  "component---cache-dev-404-page-js": preferDefault(require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/dev-404-page.js")),
+  "component---src-pages-404-js": preferDefault(require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/src/pages/404.js")),
+  "component---src-pages-index-js": preferDefault(require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/src/pages/index.js")),
+  "component---src-pages-page-2-js": preferDefault(require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/src/pages/page-2.js"))
+}
+
+exports.json = {
+  "layout-index.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/layout-index.json"),
+  "dev-404-page.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/dev-404-page.json"),
+  "layout-index.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/layout-index.json"),
+  "404.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/404.json"),
+  "layout-index.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/layout-index.json"),
+  "index.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/index.json"),
+  "layout-index.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/layout-index.json"),
+  "page-2.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/page-2.json"),
+  "layout-index.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/layout-index.json"),
+  "404-html.json": require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/json/404-html.json")
+}
+
+exports.layouts = {
+  "component---src-layouts-index-js": preferDefault(require("/Users/shoemakerdr/dev/JS/shoemakerdr.github.io/.cache/layouts/index.js"))
+}
