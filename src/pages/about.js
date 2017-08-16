636354b66e98c20de282e47cfb887fdcf1e42e50
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-15 23:10:43 -0500

    Added about page/route.

---
+++ src/pages/about.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const AboutPage = () =>
+  <div>
+    <h1>About</h1>
+    <p>
+
+I'm a front end web developer in Chicago, IL. I'm available for freelance work designing and building websites and web apps, so check out my previous projects. I also perform improv comedy at the <a target='_blank' href="https://www.ioimprov.com">iO Theatre</a> and <a target='_blank' href="https://www.cictheater.com">CIC Theater</a>.
+</p>
+    <Link to="/page-2/">Go to page 2</Link>
+  </div>
+
+export default AboutPage 
