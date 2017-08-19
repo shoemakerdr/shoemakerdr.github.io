7846cfe9c6acf05362c2284c4b5f4b28cbcdf4a8
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-19 13:16:33 -0500

    Added projects index and random quote generator page

---
+++ src/pages/projects/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const ProjectIndex = () =>
+  <div>
+    <h1>Projects</h1>
+    <Link to="/projects/random_quote_generator">Random Quote Generator</Link>
+  </div>
+
+export default ProjectIndex
---
+++ src/pages/projects/random_quote_generator.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const RandomQuote = () =>
+  <div>
+    <h1>Random Quote Generator</h1>
+    <p>This is a page describing my Random Quote Generator project</p>
+<img src='https://previews.123rf.com/images/stocking/stocking1211/stocking121100600/16599295-Portrait-of-a-thoughtful-man-having-doubts-Stock-Photo-confused-advice-man.jpg' width='192' height='120' />
+    <Link to="/projects/">Back to projects</Link>
+  </div>
+
+export default RandomQuote  
