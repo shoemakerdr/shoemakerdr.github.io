8b3e319b0a84980dc3e514050dccba412c3e366e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-19 13:16:41 -0500

    Added writing index

---
+++ src/pages/writing/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const WritingIndex = () =>
+  <div>
+    <h1>Writing</h1>
+    <Link to="/">Back to home</Link>
+  </div>
+
+export default WritingIndex 
