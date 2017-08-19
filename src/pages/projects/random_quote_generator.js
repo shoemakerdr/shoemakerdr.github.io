e3471cc335010e6c158b321237cb1c0a9a3fc6b5
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-19 14:03:45 -0500

    Replaced html with Project component.

--- src/pages/projects/random_quote_generator.js
+++ src/pages/projects/random_quote_generator.js
@@ -1,12 +1,17 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import Project from './Project'
+
+const rand = {
+    name: 'Random Quote Generator',
+    description: 'This is a page describing my Random Quote Generator project',
+    screenshot: 'https://previews.123rf.com/images/stocking/stocking1211/stocking121100600/16599295-Portrait-of-a-thoughtful-man-having-doubts-Stock-Photo-confused-advice-man.jpg'
+}
 
 const RandomQuote = () =>
-  <div>
-    <h1>Random Quote Generator</h1>
-    <p>This is a page describing my Random Quote Generator project</p>
-<img src='https://previews.123rf.com/images/stocking/stocking1211/stocking121100600/16599295-Portrait-of-a-thoughtful-man-having-doubts-Stock-Photo-confused-advice-man.jpg' width='192' height='120' />
-    <Link to="/projects/">Back to projects</Link>
-  </div>
-
+    <Project
+        name={rand.name} 
+        description={rand.description} 
+        screenshot={rand.screenshot} 
+    />
 export default RandomQuote  
