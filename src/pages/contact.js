66b14bb1497e9042a0b9dfa0bea3dd48b314f1bd
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-19 13:20:05 -0500

    Added basic contact page.

---
+++ src/pages/contact.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Contact = () =>
+  <div>
+    <h1>Contact</h1>
+    <a target='_blank' href='https://github.com/shoemakerdr'>Github</a>
+    <a target='_blank' href='https://www.linkedin.com/in/derek-shoemaker-487b92103/'>LinkedIn</a>
+  </div>
+
+export default Contact 
