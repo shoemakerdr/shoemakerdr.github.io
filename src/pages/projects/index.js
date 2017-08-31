402705019738874e1d93e0d6c7700e3ba26de677
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-30 20:18:52 -0500

    Added links to rest of projects. Imported withBlockDisplay HOC

--- src/pages/projects/index.js
+++ src/pages/projects/index.js
@@ -1,10 +1,22 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import withBlockDisplay from '../withBlockDisplay'
+
+const BlockLink = withBlockDisplay(Link)
 
 const ProjectIndex = () =>
   <div>
     <h1>Projects</h1>
-    <Link to="/projects/random_quote_generator">Random Quote Generator</Link>
+    <BlockLink to="/projects/random-quote">Random Quote Generator</BlockLink>
+    <BlockLink to="/projects/local-weather">Local Weather</BlockLink>
+    <BlockLink to="/projects/wikiviewer">Wikipedia Searcher</BlockLink>
+    <BlockLink to="/projects/twitch-tv">Twitch TV Channel App</BlockLink>
+    <BlockLink to="/projects/calculator">Calculator</BlockLink>
+    <BlockLink to="/projects/pomodoro">Pomodoro Timer</BlockLink>
+    <BlockLink to="/projects/tictactoe">Tic Tac Toe</BlockLink>
+    <BlockLink to="/projects/simon">Simon</BlockLink>
+    <BlockLink to="/projects/markdown-previewer">Markdown Previewer</BlockLink>
+    <BlockLink to="/projects/leaderboard">FreeCodeCamp Leaderboard</BlockLink>
   </div>
 
 export default ProjectIndex
