bad36fb7bfc9bd30f9c78fa02e2e24027f47c264
Derek Shoemaker <shoemakerdr@gmail.com>
2017-08-30 20:16:53 -0500

    Changed a tags to block style

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import Link from 'gatsby-link'
 
+const blockStyle = {
+    display: 'block'
+}
+
 const Contact = () =>
   <div>
     <h1>Contact</h1>
-    <a target='_blank' href='https://github.com/shoemakerdr'>Github</a>
-    <a target='_blank' href='https://www.linkedin.com/in/derek-shoemaker-487b92103/'>LinkedIn</a>
+    <a style={blockStyle} target='_blank' href='https://github.com/shoemakerdr'>Github</a>
+    <a style={blockStyle} target='_blank' href='https://www.linkedin.com/in/derek-shoemaker-487b92103/'>LinkedIn</a>
   </div>
 
 export default Contact 
