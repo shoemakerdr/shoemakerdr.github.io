5f3975ed233770c4157aa7eff264d417c692f706
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-02 14:23:16 -0500

    Added styling to Project component

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -3,17 +3,28 @@ import Link from 'gatsby-link'
 
 const styles = {
     image: {
-        display: 'block'
+        margin: 0,
+        width: '50%',
+        height: '50%'
+    },
+    flex: {
+        width: '100%',
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    info: {
+        margin:0
     }
 }
 
-const Project = props =>
-  <div>
-    <a href={props.link}><h1>{props.title}</h1></a>
-    <h3>Technologies used: {props.technologies}</h3>
-    <p>{props.description}</p>
-    <img style={styles.image} src={props.screenshot} width='350' height='350' />
-    <Link to="/projects/">Back to projects</Link>
-  </div>
+export default props =>
+    <div style={styles.flex}>
+        <div style={styles.info}>
+            <a href={props.link} target='_blank'><h1>{props.title}</h1></a>
+            <h3>Technologies used: {props.technologies}</h3>
+            <p>{props.description}</p>
+            <Link to="/projects/">Back to projects</Link>
+        </div>
+        <img style={styles.image} src={props.screenshot} />
+    </div>
 
-export default Project 
