4064c4ef6f6e2d5458a745d2160c33db2cdc54a4
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-03 17:54:55 -0500

    Added sunglasses image and styling.

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,15 +1,34 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import sunglasses from '../sunglasses.jpg'
 
-const blockStyle = {
-    display: 'block'
+const styles = {
+    link: {
+        padding: '.5em',
+        paddingTop: 0,
+    },
+    image: {
+        width: '100%',
+        maxHeight: '500px'
+    },
+    div: {
+        display: 'flex',
+        flexDirection: 'row',
+        justifyContent: 'center',
+        width: '100%'
+    }
 }
 
 const Contact = () =>
-  <div>
-    <h1>Contact</h1>
-    <a style={blockStyle} target='_blank' href='https://github.com/shoemakerdr'>Github</a>
-    <a style={blockStyle} target='_blank' href='https://www.linkedin.com/in/derek-shoemaker-487b92103/'>LinkedIn</a>
-  </div>
+    <div>
+        <h1>Contact</h1>
+        <div style={styles.div}>
+            <a style={styles.link} target='_blank' href='https://github.com/shoemakerdr'>Github</a>
+            <a style={styles.link} target='_blank' href='https://www.linkedin.com/in/derek-shoemaker-487b92103/'>LinkedIn</a>
+        </div>
+        <div>
+            <img style={styles.image} src={sunglasses} alt='sunglasses' />
+        </div>
+    </div>
 
 export default Contact 
