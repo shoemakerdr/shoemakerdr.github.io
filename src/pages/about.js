7d9b1de0b033319bcbdf293a01a35d5fab5c1d40
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-03 17:52:53 -0500

    Added headshot image and styling.

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,13 +1,31 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import headshot from '../headshot.jpg'
+
+const styles = {
+    div: {
+        display: 'flex',
+        flexDirection: 'row',
+    },
+    image: {
+        position: 'relative',
+        maxWidth: '900px',
+        maxHeight: '500px'
+    },
+    para: {
+        paddingLeft: '2.5em',
+        paddingTop: '9.5em'
+    }
+}
 
 const AboutPage = () =>
-  <div>
-    <h1>About</h1>
-    <p>
-
-I'm a front end web developer in Chicago, IL. I'm available for freelance work designing and building websites and web apps, so check out my previous projects. I also perform improv comedy at the <a target='_blank' href="https://www.ioimprov.com">iO Theatre</a> and <a target='_blank' href="https://www.cictheater.com">CIC Theater</a>.
-</p>
-  </div>
+    <div>
+        <h1>About</h1>
+        <p>I'm a web developer in Chicago, IL. I'm available for freelance work designing and building websites and web apps.</p>
+        <div style={styles.div} >
+            <img style={styles.image} src={headshot} alt='headshot' />
+            <p style={styles.para}>I also perform improv comedy at the <a target='_blank' href="https://www.ioimprov.com">iO Theatre</a> and <a target='_blank' href="https://www.cictheater.com">CIC Theater</a>.</p>
+        </div>
+    </div>
 
 export default AboutPage 
