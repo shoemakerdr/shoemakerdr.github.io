48853119fa1496b48d95d455b17c7cb3bfd88130
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-03 17:54:04 -0500

    Added cherub photo and styling

--- src/pages/writing/index.js
+++ src/pages/writing/index.js
@@ -1,10 +1,38 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import cherub from '../../cherub.jpg'
+
+const styles = {
+    div: {
+        display: 'flex',
+        flexDirection: 'row',
+        width: '100%'
+    },
+    image: {
+        position: 'relative',
+        maxWidth: '900px',
+        maxHeight: '500px'
+    },
+    paraDiv: {
+        width: '50%',
+        paddingRight: '2.5em',
+        paddingTop: '9.5em'
+    },
+    text: {
+        paddingTop: '3em'
+    }
+}
 
 const WritingIndex = () =>
-  <div>
-    <h1>Writing</h1>
-    <Link to="/">Back to home</Link>
-  </div>
+    <div>
+        <h1>Writing</h1>
+        <div style={styles.div} >
+            <div style={styles.paraDiv}>
+                <p>Nothing here yet.</p>
+                <p style={styles.text}>Nothing except this weird picture with no context...</p>
+            </div>
+            <img style={styles.image} src={cherub} alt='cherub' />
+        </div>
+    </div>
 
 export default WritingIndex 
