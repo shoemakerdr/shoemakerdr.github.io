0c43921fb911cd3053ebc8c7bb825154413f5b5c
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-02 20:27:32 -0500

    Added Who Should Walk Gator page

---
+++ src/pages/projects/who-should-walk-gator.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Project from './Project'
+
+const data = {
+    title: 'Who Should Walk Gator?',
+    link: 'https://who-should-walk-gator.herokuapp.com',
+    technologies: 'Python, Flask, Google Sheets (as a database), Oauth2Client',
+    description: 'This is a page describing my Who Should Walk Gator? project',
+    screenshot: 'https://previews.123rf.com/images/stocking/stocking1211/stocking121100600/16599295-Portrait-of-a-thoughtful-man-having-doubts-Stock-Photo-confused-advice-man.jpg'
+}
+
+export default () =>
+    <Project {...data} />
+
