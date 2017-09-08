930ce842f103e88ce88c2e328812d59207500996
Derek Shoemaker <shoemakerdr@gmail.com>
2017-09-08 14:52:24 -0500

    Added styling and onClick to image. Added padding to info.

--- src/pages/projects/Project.js
+++ src/pages/projects/Project.js
@@ -5,7 +5,9 @@ const styles = {
     image: {
         margin: 0,
         width: '50%',
-        height: '50%'
+        height: '50%',
+        cursor: 'pointer',
+        boxShadow: '0 14px 28px rgba(0,0,0,0.25), 0 10px 10px rgba(0,0,0,0.22)'
     },
     flex: {
         width: '100%',
@@ -14,7 +16,8 @@ const styles = {
     },
     info: {
         margin:0,
-        paddingTop: '.25em'
+        paddingTop: '.25em',
+        paddingRight: '.5em'
     },
     link: {
         marginTop: '1em',
@@ -40,7 +43,7 @@ export default props =>
                 {props.additionalLinks && <div style={styles.additional}>{props.additionalLinks.map(link => <a href={link.href} target='_blank'>{link.text}</a>)}</div>}
                 <a href={props.source} target='_blank'>Source code on Github</a>
             </div>
-            <img style={styles.image} src={props.screenshot} />
+            <img onClick={() => window.open(props.link, '_blank')} style={styles.image} src={props.screenshot} />
         </div>
         <div style={styles.link}>
             <Link to="/projects/" >Back to projects</Link>
