15f7f17a47df063f8fa04832ceba400b88cccc71
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-01 13:53:22 -0600

    Added projects page

---
+++ src/pages/projects.js
@@ -0,0 +1,14 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const IndexPage = () =>
+  <div>
+    <h1>Welcome</h1>
+    <div>
+        <div>
+            <h3>Projects</h3>
+            <Link to='/' >Home</Link>
+        </div>
+    </div>
+  </div>
+export default IndexPage
