71e8976d569a5b213a948315c2d343c81f715017
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-01 13:54:04 -0600

    Extracted components and moved to component folder

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import Link from 'gatsby-link'
+
+const Footer = () =>
+    <div>
+        <p style={{marginBottom:0, marginTop: '2em', paddingBottom: '.25em'}}>Copyright 2017 by Derek Shoemaker | Made with <a href='https://facebook.github.io/react' target='_blank'>React</a> and <a href='https://gatsbyjs.org' target='_blank'>GatsbyJS</a></p>
+    </div>
+
+export default Footer
---
+++ src/components/Header.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Link from 'gatsby-link'
+import styles from '../styles/components/Header.module.css'
+
+const Header = () =>
+  <div className={styles.icon}>
+      &#9776;
+  </div>
+
+export default Header
