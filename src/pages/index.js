b1628d11d9ae1f4b853619a013f502b91e082718
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-02 01:52:45 -0600

    Added all text, image, and icons to homepage

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,21 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import readingSketch from '../images/readingSketch.jpg'
+import styles from '../styles/pages/index.module.css'
 
 const IndexPage = () =>
   <div>
-    <h1>Welcome</h1>
     <div>
         <div>
-            <h3>My name is Derek.</h3>
-            <Link to='/projects' >You can click here to check out some projects I've built.</Link>
+            <h2 className={styles.story}>Does your website tell a story?</h2>
+            <img className={styles.image} src={readingSketch} title='fairy tale' />
+            <h1 className={styles.name}>Derek Shoemaker</h1>
+            <h3 className={styles.developer}>web developer</h3>
+            <div className={styles.icons}>
+                <a href='https://github.com/shoemakerdr' className={styles.socialIcon}><i className='fa fa-github'></i></a>
+                <a href='https://twitter.com/derekrshoe' className={styles.socialIcon}><i className='fa fa-twitter'></i></a>
+                <a href='https://www.linkedin.com/in/derek-shoemaker-487b92103/' className={styles.socialIcon}><i className='fa fa-linkedin'></i></a>
+            </div>
         </div>
     </div>
   </div>
