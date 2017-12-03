494b6de5621356a90812c4321b90c2b2dc62e1fc
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-03 14:59:59 -0600

    Changed TemplateWrapper to class

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,23 +1,49 @@
-import React from 'react'
+import React, { Component } from 'react'
 import Helmet from 'react-helmet'
+import Menu from '../components/Menu'
 import Header from '../components/Header'
 import Footer from '../components/Footer'
 
 import styles from '../styles/layout/index.module.css'
+import './index.css'
 
-const TemplateWrapper = ({ children }) =>
-  <div className={styles.wrapper}>
-    <Helmet
-      title="Derek Shoemaker | Portfolio"
-      meta={[
-        { name: 'description', content: 'Development Portfolio of Derek Shoemaker' },
-        { name: 'keywords', content: 'web developer, web development, front end, full stack, react, javascript, freelance' },
-      ]}
-    />
-    <Header />
-    <div>
-      {children()}
-    </div>
-  </div>
+class TemplateWrapper extends Component { 
+    constructor (props) {
+        super(props)
+        this.toggleMenu = this.toggleMenu.bind(this)
+        this.children = props.children
+        this.state = {
+            isMenuOpen: false,
+        }
+    }
 
+    toggleMenu () {
+        this.setState(prevState => {
+            console.log(!prevState.isMenuOpen)
+            return {
+                isMenuOpen: !prevState.isMenuOpen
+            }
+        })
+    }
+
+    render () {
+        return (
+            <div className={styles.wrapper}>
+                <Helmet
+                    title="Derek Shoemaker | Portfolio"
+                    meta={[
+                    { name: 'description', content: 'Development Portfolio of Derek Shoemaker' },
+                    { name: 'keywords', content: 'web developer, web development, front end, full stack, react, javascript, freelance' },
+                    ]}
+                />
+                <Menu isMenuOpen={this.state.isMenuOpen} toggleMenu={this.toggleMenu} />
+                <Header isMenuOpen={this.state.isMenuOpen} toggleMenu={this.toggleMenu}/>
+                <div>
+                    {this.children()}
+                </div>
+            </div>
+        )
+    }
+}
+  
 export default TemplateWrapper
