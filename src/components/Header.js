c82bf46e7136466b875cd5ea7b57360036106c13
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-03 15:02:15 -0600

    Added style class and onClick method to close button

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,9 +2,9 @@ import React from 'react'
 import Link from 'gatsby-link'
 import styles from '../styles/components/Header.module.css'
 
-const Header = () =>
-  <div className={styles.icon}>
-      &#9776;
-  </div>
+const Header = props =>
+    <div className={styles.icon}>
+        <div onClick={props.toggleMenu} className={props.isMenuOpen ? styles.hidden : styles.visible}>&#9776;</div>
+    </div>
 
 export default Header
