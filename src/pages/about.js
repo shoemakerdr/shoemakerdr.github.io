89d4630d5c6dbf3804c4467dacc8d5c819658815
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-06 13:53:05 -0600

    Added new about page content

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,12 +1,30 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import crowsSketch from '../images/crowsSketch.jpg'
+import styles from '../styles/pages/about.module.css'
+
+const StyledLink = props =>
+    <Link className={`${styles.contact} ${styles.link}`} to={props.to} onClick={props.onClick}>{props.children}</Link>
 
 const AboutPage = () =>
     <div>
-        <h1>About</h1>
-        <p>I'm a web developer in Chicago, IL. I'm available for freelance work designing and building websites and web apps.</p>
-        <div>
-            <p>I also perform improv comedy at the <a target='_blank' href="https://www.ioimprov.com">iO Theatre</a> and <a target='_blank' href="https://www.cictheater.com">CIC Theater</a>.</p>
+        <img src={crowsSketch} className={styles.image}/>
+        <div className={styles.wrapper}>
+            <h1 className={styles.heading}>About</h1>
+            <p className={styles.text}>
+                Derek Shoemaker is a web developer living in Chicago.
+                He also performs 
+                at <a
+                    className={styles.link}
+                    target='_blank'
+                    href='https://www.ioimprov.com'
+                >iO Theatre</a> and <a
+                    className={styles.link}
+                    target='_blank'
+                    href='https://www.cictheater.com'
+                >CIC Theater</a>.
+            </p>
+            <StyledLink to='/contact'>Contact for freelance opportunities</StyledLink>
         </div>
     </div>
 
