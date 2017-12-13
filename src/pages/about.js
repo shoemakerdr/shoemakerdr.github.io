7b30caed8baf3b1b0d18f53aeac5addd4273e422
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-13 13:54:30 -0600

    Moved crowSketch into wrapper.

--- src/pages/about.js
+++ src/pages/about.js
@@ -7,25 +7,23 @@ const StyledLink = props =>
     <Link className={`${styles.contact} ${styles.link}`} to={props.to} onClick={props.onClick}>{props.children}</Link>
 
 const AboutPage = () =>
-    <div>
+    <div className={styles.contentWrapper}>
         <img src={crowsSketch} className={styles.image}/>
-        <div className={styles.wrapper}>
-            <h1 className={styles.heading}>About</h1>
-            <p className={styles.text}>
-                Derek Shoemaker is a web developer living in Chicago.
-                He also performs 
-                at <a
-                    className={styles.link}
-                    target='_blank'
-                    href='https://www.ioimprov.com'
-                >iO Theatre</a> and <a
-                    className={styles.link}
-                    target='_blank'
-                    href='https://www.cictheater.com'
-                >CIC Theater</a>.
-            </p>
-            <StyledLink to='/contact'>Contact for freelance opportunities</StyledLink>
-        </div>
+        <h1 className={styles.heading}>About</h1>
+        <p className={styles.text}>
+            Derek Shoemaker is a web developer living in Chicago.
+            He also performs 
+            at <a
+                className={styles.link}
+                target='_blank'
+                href='https://www.ioimprov.com'
+            >iO Theatre</a> and <a
+                className={styles.link}
+                target='_blank'
+                href='https://www.cictheater.com'
+            >CIC Theater</a>.
+        </p>
+        <StyledLink to='/contact'>Contact for freelance opportunities</StyledLink>
     </div>
 
 export default AboutPage 
