6c8b2685856e170ccb144ce3a7761f1e61980105
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-13 23:31:23 -0600

    Add project data

---
+++ src/project-list.js
@@ -0,0 +1,35 @@
+import votr from './images/votr-screenshot.jpeg'
+import calculator from './images/calculator-screenshot.jpeg'
+import tictactoe from './images/tictactoe-screenshot.jpeg'
+import gator from './images/gator-screenshot.jpeg'
+
+export default [
+    {
+        screenshot: votr,
+        title: 'Votr',
+        description: 'Fullstack app using React, Node, Express, and Postgres to create, vote and share polls',
+        link: 'https://shoemakerdr.github.io/votr',
+        code: 'https://github.com/shoemakerdr/votr',
+    },
+    {
+        screenshot: calculator,
+        title: 'Javascript Calculator',
+        description: '',
+        link: 'https://shoemakerdr.github.io/Calculator',
+        code: 'https://github.com/shoemakerdr/Calculator',
+    },
+    {
+        screenshot: tictactoe,
+        title: 'Tic Tac Toe',
+        description: 'Tic Tac Toe with an unbeatable computer opponent',
+        link: 'https://shoemakerdr.github.io/TicTacToe',
+        code: 'https://github.com/shoemakerdr/TicTacToe',
+    },
+    {
+        screenshot: gator,
+        title: 'Who Should Walk Gator',
+        description: 'An app built with Python and Google Sheets to show Gator\'s walking schedule',
+        link: 'https://who-should-walk-gator.herokuapp.com/',
+        code: 'https://github.com/shoemakerdr/who-should-walk-gator',
+    },
+]
