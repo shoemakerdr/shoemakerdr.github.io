04559b030cc5ca45accbf6a62ee4cfff2079d309
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-13 19:19:34 -0600

    Added initial work page component

--- src/pages/work.js
+++ src/pages/work.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Link from 'gatsby-link'
+import leafSketch from '../images/leafSketch.jpg'
+import styles from '../styles/pages/work.module.css'
 
-const IndexPage = () =>
-  <div>
-    <h1>Welcome</h1>
-    <div>
-        <div>
-            <h3>Projects</h3>
-            <Link to='/' >Home</Link>
-        </div>
+const WorkPage = () =>
+    <div className={styles.contentWrapper}>
+        <img src={leafSketch} className='image'/>
+        <h1 className='heading'>Work</h1>
+        <p className={styles.text}>
+            Projects Go Here
+        </p>
     </div>
-  </div>
-export default IndexPage
+export default WorkPage
