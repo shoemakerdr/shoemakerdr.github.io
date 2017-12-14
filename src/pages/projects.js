54a43a365a2f316bdae4adc476698fa3d0f8c4c3
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-13 23:56:57 -0600

    Added responsive wrapper around project list

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -7,12 +7,12 @@ import styles from '../styles/pages/projects.module.css'
 import projectList from '../project-list'
 
 const ProjectsPage = () =>
-    <div className={styles.contentWrapper}>
+    <div className={styles.wrapper}>
         <img src={leafSketch} className='image'/>
         <h1 className='heading'>Projects</h1>
         <div>
             {projectList.length > 0 && 
-            <div>
+            <div className={styles.projectsWrapper}>
                 {projectList.map((project, i) =>
                     <Project info={project} key={i} />
                 )}
