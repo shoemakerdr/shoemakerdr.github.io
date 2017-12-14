0cd7dce10cf00008de433db61d25a814ce0b808e
Derek Shoemaker <shoemakerdr@gmail.com>
2017-12-13 23:33:08 -0600

    Added more info and styling to about page. Changed  link to route to /projects instead of /work

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,8 @@ import Link from 'gatsby-link'
 import crowsSketch from '../images/crowsSketch.jpg'
 import styles from '../styles/pages/about.module.css'
 
-// const StyledLink = props =>
-//     <Link className={`${styles.contact} ${styles.link}`} to={props.to} onClick={props.onClick}>{props.children}</Link>
+const StyledLink = props =>
+    <Link className={`${styles.contact} ${styles.link}`} to={props.to} onClick={props.onClick}>{props.children}</Link>
 
 const AboutPage = () =>
     <div className={styles.contentWrapper}>
@@ -12,7 +12,12 @@ const AboutPage = () =>
         <h1 className='heading'>About</h1>
         <p className={styles.text}>
             Derek Shoemaker is a web developer living in Chicago.
-            He also performs 
+            He is experienced with numerous languages and technologies,  
+            including Javascript, React, NodeJS, Python, 
+            HTML, CSS, Photoshop, and Git/Github.
+        </p>
+        <p className={styles.text}>
+            Derek performs 
             at <a
                 className={styles.link}
                 target='_blank'
@@ -23,7 +28,7 @@ const AboutPage = () =>
                 href='https://www.cictheater.com'
             >CIC Theater</a>.
         </p>
-        {/* <StyledLink to='/contact'>Contact for freelance opportunities</StyledLink> */}
+        <StyledLink to='/projects'>See previous work</StyledLink>
     </div>
 
 export default AboutPage 
